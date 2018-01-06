a33cfb6ec51c2319405c4bd0026ba8db9b81e46a
JoanShadow <joanpecamp1993@gmail.com>
2018-01-06 16:08:42 +0100

    first-commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var box_cortina = document.getElementById("box_cortina");
+
+function onAnimation() {
+
+	var tapa_cortina = document.getElementById("tapa_cortina");
+	var box_cortina = document.getElementById("box_cortina");
+
+	box_cortina.classList.add("fade_cortina");
+	tapa_cortina.classList.add("tapa_cortina");
+}
